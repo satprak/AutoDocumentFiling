8a51e95e8efb75017826382ec4f241599b0b2672
Satyam <satyamprakashiitk2022@gmail.com>
2021-07-08 20:05:01 +0530

    Final

--- adf_main/adf_frontend/settings.py
+++ adf_main/adf_frontend/settings.py
@@ -15,7 +15,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -158,3 +158,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 #............#
 CORS_ORIGIN_ALLOW_ALL = True
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
