2b2cdd245231c1d236484748542344c02dadda4c
Satyam <satyamprakashiitk2022@gmail.com>
2021-07-09 00:59:45 +0530

    f

--- adf_main/adf_frontend/settings.py
+++ adf_main/adf_frontend/settings.py
@@ -161,9 +161,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': config('saty', default=""),
-    'API_KEY': config('672179415177664', default=""),
-    'API_SECRET': config('n0EjKl9pwAGyScvr7c4-iAKEzkg', default=""),
+    'CLOUD_NAME': "saty",
+    'API_KEY': '672179415177664',
+    'API_SECRET': 'n0EjKl9pwAGyScvr7c4-iAKEzkg',
 }
 
 
